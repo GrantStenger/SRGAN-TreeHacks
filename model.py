ecee89debc8bf4f88fe0cc3526151b5d96cbb091
Tyler LaBonte <tlabonte@usc.edu>
2019-02-16 11:17:45 -0800

    fixed critical upsample error

--- model.py
+++ model.py
@@ -44,107 +44,107 @@ def SRGAN_g(t_image, is_train=False, reuse=False):
         n = Conv2d(n, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/1')
         n = SubpixelConv2d(n, scale=2, n_out_channel=None, act=tf.nn.relu, name='pixelshufflerx2/1')
 
-        n = Conv2d(n, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/2')
-        n = SubpixelConv2d(n, scale=2, n_out_channel=None, act=tf.nn.relu, name='pixelshufflerx2/2')
+        #n = Conv2d(n, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/2')
+        #n = SubpixelConv2d(n, scale=2, n_out_channel=None, act=tf.nn.relu, name='pixelshufflerx2/2')
 
         n = Conv2d(n, 3, (1, 1), (1, 1), act=tf.nn.tanh, padding='SAME', W_init=w_init, name='out')
 
         return n
 
 
-def SRGAN_g2(t_image, is_train=False, reuse=False):
-    """ Generator in Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network
-    feature maps (n) and stride (s) feature maps (n) and stride (s)
-
-    96x96 --> 384x384
-
-    Use Resize Conv
-    """
-    w_init = tf.random_normal_initializer(stddev=0.02)
-    b_init = None  # tf.constant_initializer(value=0.0)
-    g_init = tf.random_normal_initializer(1., 0.02)
-    size = t_image.get_shape().as_list()
-    with tf.variable_scope("SRGAN_g", reuse=reuse) as vs:
-        # tl.layers.set_name_reuse(reuse) # remove for TL 1.8.0+
-        n = InputLayer(t_image, name='in')
-        n = Conv2d(n, 64, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init, name='n64s1/c')
-        temp = n
-
-        # B residual blocks
-        for i in range(16):
-            nn = Conv2d(n, 64, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, b_init=b_init, name='n64s1/c1/%s' % i)
-            nn = BatchNormLayer(nn, act=tf.nn.relu, is_train=is_train, gamma_init=g_init, name='n64s1/b1/%s' % i)
-            nn = Conv2d(nn, 64, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, b_init=b_init, name='n64s1/c2/%s' % i)
-            nn = BatchNormLayer(nn, is_train=is_train, gamma_init=g_init, name='n64s1/b2/%s' % i)
-            nn = ElementwiseLayer([n, nn], tf.add, name='b_residual_add/%s' % i)
-            n = nn
-
-        n = Conv2d(n, 64, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, b_init=b_init, name='n64s1/c/m')
-        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n64s1/b/m')
-        n = ElementwiseLayer([n, temp], tf.add, name='add3')
-        # B residual blacks end
-
-        # n = Conv2d(n, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/1')
-        # n = SubpixelConv2d(n, scale=2, n_out_channel=None, act=tf.nn.relu, name='pixelshufflerx2/1')
-        #
-        # n = Conv2d(n, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/2')
-        # n = SubpixelConv2d(n, scale=2, n_out_channel=None, act=tf.nn.relu, name='pixelshufflerx2/2')
-
-        ## 0, 1, 2, 3 BILINEAR NEAREST BICUBIC AREA
-        n = UpSampling2dLayer(n, size=[size[1] * 2, size[2] * 2], is_scale=False, method=1, align_corners=False, name='up1/upsample2d')
-        n = Conv2d(n, 64, (3, 3), (1, 1), padding='SAME', W_init=w_init, b_init=b_init, name='up1/conv2d')  # <-- may need to increase n_filter
-        n = BatchNormLayer(n, act=tf.nn.relu, is_train=is_train, gamma_init=g_init, name='up1/batch_norm')
-
-        n = UpSampling2dLayer(n, size=[size[1] * 4, size[2] * 4], is_scale=False, method=1, align_corners=False, name='up2/upsample2d')
-        n = Conv2d(n, 32, (3, 3), (1, 1), padding='SAME', W_init=w_init, b_init=b_init, name='up2/conv2d')  # <-- may need to increase n_filter
-        n = BatchNormLayer(n, act=tf.nn.relu, is_train=is_train, gamma_init=g_init, name='up2/batch_norm')
-
-        n = Conv2d(n, 3, (1, 1), (1, 1), act=tf.nn.tanh, padding='SAME', W_init=w_init, name='out')
-        return n
-
-
-def SRGAN_d2(t_image, is_train=False, reuse=False):
-    """ Discriminator in Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network
-    feature maps (n) and stride (s) feature maps (n) and stride (s)
-    """
-    w_init = tf.random_normal_initializer(stddev=0.02)
-    b_init = None
-    g_init = tf.random_normal_initializer(1., 0.02)
-    lrelu = lambda x: tl.act.lrelu(x, 0.2)
-    with tf.variable_scope("SRGAN_d", reuse=reuse) as vs:
-        # tl.layers.set_name_reuse(reuse) # remove for TL 1.8.0+
-        n = InputLayer(t_image, name='in')
-        n = Conv2d(n, 64, (3, 3), (1, 1), act=lrelu, padding='SAME', W_init=w_init, name='n64s1/c')
-
-        n = Conv2d(n, 64, (3, 3), (2, 2), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n64s2/c')
-        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n64s2/b')
-
-        n = Conv2d(n, 128, (3, 3), (1, 1), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n128s1/c')
-        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n128s1/b')
-
-        n = Conv2d(n, 128, (3, 3), (2, 2), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n128s2/c')
-        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n128s2/b')
-
-        n = Conv2d(n, 256, (3, 3), (1, 1), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n256s1/c')
-        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n256s1/b')
-
-        n = Conv2d(n, 256, (3, 3), (2, 2), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n256s2/c')
-        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n256s2/b')
-
-        n = Conv2d(n, 512, (3, 3), (1, 1), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n512s1/c')
-        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n512s1/b')
-
-        n = Conv2d(n, 512, (3, 3), (2, 2), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n512s2/c')
-        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n512s2/b')
-
-        n = FlattenLayer(n, name='f')
-        n = DenseLayer(n, n_units=1024, act=lrelu, name='d1024')
-        n = DenseLayer(n, n_units=1, name='out')
-
-        logits = n.outputs
-        n.outputs = tf.nn.sigmoid(n.outputs)
-
-        return n, logits
+#def SRGAN_g2(t_image, is_train=False, reuse=False):
+#    """ Generator in Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network
+#    feature maps (n) and stride (s) feature maps (n) and stride (s)
+#
+#    96x96 --> 384x384
+#
+#    Use Resize Conv
+#    """
+#    w_init = tf.random_normal_initializer(stddev=0.02)
+#    b_init = None  # tf.constant_initializer(value=0.0)
+#    g_init = tf.random_normal_initializer(1., 0.02)
+#    size = t_image.get_shape().as_list()
+#    with tf.variable_scope("SRGAN_g", reuse=reuse) as vs:
+#        # tl.layers.set_name_reuse(reuse) # remove for TL 1.8.0+
+#        n = InputLayer(t_image, name='in')
+#        n = Conv2d(n, 64, (3, 3), (1, 1), act=tf.nn.relu, padding='SAME', W_init=w_init, name='n64s1/c')
+#        temp = n
+#
+#        # B residual blocks
+#        for i in range(16):
+#            nn = Conv2d(n, 64, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, b_init=b_init, name='n64s1/c1/%s' % i)
+#            nn = BatchNormLayer(nn, act=tf.nn.relu, is_train=is_train, gamma_init=g_init, name='n64s1/b1/%s' % i)
+#            nn = Conv2d(nn, 64, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, b_init=b_init, name='n64s1/c2/%s' % i)
+#            nn = BatchNormLayer(nn, is_train=is_train, gamma_init=g_init, name='n64s1/b2/%s' % i)
+#            nn = ElementwiseLayer([n, nn], tf.add, name='b_residual_add/%s' % i)
+#            n = nn
+#
+#        n = Conv2d(n, 64, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, b_init=b_init, name='n64s1/c/m')
+#        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n64s1/b/m')
+#        n = ElementwiseLayer([n, temp], tf.add, name='add3')
+#        # B residual blacks end
+#
+#        # n = Conv2d(n, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/1')
+#        # n = SubpixelConv2d(n, scale=2, n_out_channel=None, act=tf.nn.relu, name='pixelshufflerx2/1')
+#        #
+#        # n = Conv2d(n, 256, (3, 3), (1, 1), act=None, padding='SAME', W_init=w_init, name='n256s1/2')
+#        # n = SubpixelConv2d(n, scale=2, n_out_channel=None, act=tf.nn.relu, name='pixelshufflerx2/2')
+#
+#        ## 0, 1, 2, 3 BILINEAR NEAREST BICUBIC AREA
+#        n = UpSampling2dLayer(n, size=[size[1] * 2, size[2] * 2], is_scale=False, method=1, align_corners=False, name='up1/upsample2d')
+#        n = Conv2d(n, 64, (3, 3), (1, 1), padding='SAME', W_init=w_init, b_init=b_init, name='up1/conv2d')  # <-- may need to increase n_filter
+#        n = BatchNormLayer(n, act=tf.nn.relu, is_train=is_train, gamma_init=g_init, name='up1/batch_norm')
+#
+#        n = UpSampling2dLayer(n, size=[size[1] * 4, size[2] * 4], is_scale=False, method=1, align_corners=False, name='up2/upsample2d')
+#        n = Conv2d(n, 32, (3, 3), (1, 1), padding='SAME', W_init=w_init, b_init=b_init, name='up2/conv2d')  # <-- may need to increase n_filter
+#        n = BatchNormLayer(n, act=tf.nn.relu, is_train=is_train, gamma_init=g_init, name='up2/batch_norm')
+#
+#        n = Conv2d(n, 3, (1, 1), (1, 1), act=tf.nn.tanh, padding='SAME', W_init=w_init, name='out')
+#        return n
+#
+#
+#def SRGAN_d2(t_image, is_train=False, reuse=False):
+#    """ Discriminator in Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network
+#    feature maps (n) and stride (s) feature maps (n) and stride (s)
+#    """
+#    w_init = tf.random_normal_initializer(stddev=0.02)
+#    b_init = None
+#    g_init = tf.random_normal_initializer(1., 0.02)
+#    lrelu = lambda x: tl.act.lrelu(x, 0.2)
+#    with tf.variable_scope("SRGAN_d", reuse=reuse) as vs:
+#        # tl.layers.set_name_reuse(reuse) # remove for TL 1.8.0+
+#        n = InputLayer(t_image, name='in')
+#        n = Conv2d(n, 64, (3, 3), (1, 1), act=lrelu, padding='SAME', W_init=w_init, name='n64s1/c')
+#
+#        n = Conv2d(n, 64, (3, 3), (2, 2), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n64s2/c')
+#        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n64s2/b')
+#
+#        n = Conv2d(n, 128, (3, 3), (1, 1), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n128s1/c')
+#        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n128s1/b')
+#
+#        n = Conv2d(n, 128, (3, 3), (2, 2), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n128s2/c')
+#        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n128s2/b')
+#
+#        n = Conv2d(n, 256, (3, 3), (1, 1), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n256s1/c')
+#        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n256s1/b')
+#
+#        n = Conv2d(n, 256, (3, 3), (2, 2), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n256s2/c')
+#        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n256s2/b')
+#
+#        n = Conv2d(n, 512, (3, 3), (1, 1), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n512s1/c')
+#        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n512s1/b')
+#
+#        n = Conv2d(n, 512, (3, 3), (2, 2), act=lrelu, padding='SAME', W_init=w_init, b_init=b_init, name='n512s2/c')
+#        n = BatchNormLayer(n, is_train=is_train, gamma_init=g_init, name='n512s2/b')
+#
+#        n = FlattenLayer(n, name='f')
+#        n = DenseLayer(n, n_units=1024, act=lrelu, name='d1024')
+#        n = DenseLayer(n, n_units=1, name='out')
+#
+#        logits = n.outputs
+#        n.outputs = tf.nn.sigmoid(n.outputs)
+#
+#        return n, logits
 
 
 def SRGAN_d(input_images, is_train=True, reuse=False):

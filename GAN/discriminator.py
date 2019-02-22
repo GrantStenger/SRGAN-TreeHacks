c15022d054ba26789212499f2d423b0e0c57ae7e
Tyler LaBonte <tmlabonte@gmail.com>
2019-02-22 01:30:32 -0800

    Commenting

--- GAN/discriminator.py
+++ GAN/discriminator.py
@@ -1,8 +1,24 @@
+""" Defines the discriminator neural network for the GAN. """
+
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten
 
 
 def create_discriminator(input_shape):
+    """ Creates the discriminator for the GAN.
+
+        Instantiates a Keras model which performs two convolutions
+        on the input, then feeds it into a dense layer to determine
+        whether the given image is real or generated.
+
+        Args:
+            input_shape: A triple (x, y, channels) representing the input shape.
+
+        Returns:
+            model: The created Keras model.
+
+    """
+
     model = Sequential()
     model.name = "discriminator"
 
